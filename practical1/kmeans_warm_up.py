87945ced936275ac79075dec16a1a6b60b2433a1
Rebecca Chen <rchen@college.harvard.edu>
2014-02-02 18:35:32 -0500

    added get_image function

--- practical1/kmeans_warm_up.py
+++ practical1/kmeans_warm_up.py
@@ -1,32 +1,52 @@
 import numpy as np
 import util
 import random
+import cPickle
+from PIL import Image
+
+def get_image(cifar_array, name):
+    image_array = np.zeros((32,32,3), np.uint8)
+    for i in range(32):
+        for j in range(32):
+            rgb = np.zeros(3, np.uint8)
+            for k in range(3):
+                rgb[k] = cifar_array[(i*32) + (j%32) + (k*1024)]
+            image_array[i,j] = rgb
+    img = Image.fromarray(image_array)
+    img.save(name)
 
 #matrix as list of column vector
+def get_distances(k, num_pts, mat, mu):
+    fst = np.ones((num_pts,k))*np.sum(mat * mat, axis = 1).reshape(num_pts,1)
+    product = np.dot(mat, np.transpose(mu))
+    snd = np.ones((num_pts,k))*np.sum(mu*mu, axis = 1)
+    result = fst - 2*product + snd
+    return result
 
 def kmeans(mat, k):
+    mat = mat.astype(float)
     num_pts = mat.shape[0]
     dim = mat.shape[1]
     resp = np.zeros((num_pts,1))
     for i in range(num_pts):
     	num = random.randrange(k)
 	resp[i][0]=num
-    print resp
     mu = np.zeros((k,dim))
-    result = np.zeros((num_pts,k))
-    while(True):
+    #while(True):
+    for t in range(5):
         for i in range(k):
     	    mu[i] = np.mean(mat[resp[:,0]==i,:],axis=0)
-    	fst = np.ones((num_pts,k))*np.sum(mat * mat, axis = 1).reshape(num_pts,1)
-	product = np.dot(mat, np.transpose(mu))
-	snd = np.ones((num_pts,k))*np.sum(mu*mu, axis = 1)
-	result = fst - 2*product + snd
+
+        result = get_distances(k, num_pts, mat, mu)
 	temp_resp = np.argmin(result,axis = 1).reshape(num_pts,1)
-	print sum(np.min(result,axis = 1))
-        print result
         if(temp_resp == resp).all():
 	     break
 	resp = temp_resp
-    return sum(np.min(result,axis = 1))
-     
-print kmeans(np.arange(100).reshape(10,10),3)
+        print sum(np.min(result,axis = 1))
+    for i in range(k):
+        get_image(mu[i], str(i) + ".png")
+
+fo = open('cifar-10-batches-py/data_batch_1', 'rb')
+dict = cPickle.load(fo)
+fo.close()
+kmeans(dict['data'], 5)
