5187fccd31f8a146c5851965cadcdac22bb3df7a
Aaron Landesman <aaronlandesman@gmail.com>
2014-02-07 18:22:17 -0500

    started matrix

---
+++ practical1/make_matrix.py
@@ -0,0 +1,27 @@
+from scipy.sparse import coo_matrix
+import numpy as np
+import util
+
+pred_filename  = 'pred-weighted-book-user-mean6.csv'
+train_filename = 'ratings-train.csv'
+test_filename  = 'ratings-test.csv'
+book_filename  = 'books.csv'
+user_filename  = 'users.csv'
+
+training_data  = util.load_train(train_filename)
+test_queries   = util.load_test(test_filename)
+book_list      = util.load_books(book_filename)
+user_list      = util.load_users(user_filename)
+
+num_train = len(training_data)
+mean_rating = float(sum(map(lambda x: x['rating'], training_data)))/num_train
+
+data = np.zeros((num_train))
+book_num  = np.zeros((num_train))
+user_id = np.zeros((num_train))
+for i in range num_train:
+    data[i] = training_data[i]['rating']
+    book_num[i] = training_data[i]['index']
+    user_id[i] = training_data[i]['user']
+
+mat = coo_matrix((data,(book_num,user_id)),[shape=(len(book_list),len(user_list)])
