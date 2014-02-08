f3d739e6229cb5691bd43ac9f25462e847b14f0f
Rebecca Chen <rchen@college.harvard.edu>
2014-02-07 22:38:03 -0500

    index to user id and index to isbn dictionaries

--- practical1/predictor.py
+++ practical1/predictor.py
@@ -66,12 +66,14 @@ index = 0
 for book in book_short:
     book_keys[book['isbn']] = index
     index += 1
+inv_book_keys = {index:isbn for isbn,index in book_keys.items()}
 
 user_keys = {}
 index = 0
 for user in user_short:
     user_keys[user['user']] = index
     index += 1
+inv_user_keys = {index:user for user,index in user_keys.items()}
 
 mat = np.zeros((num_users, num_books))
 # book_pref = 0
@@ -82,11 +84,12 @@ for rating in train_short:
         book_pref = float(book['total']) / book['count']    
     else:
         book_pref = mean_rating
-    mat[user_keys[rating['user']]][book_keys[rating['isbn']]] = rating['rating'] - float(user['total']) / user['count'] + mean_rating - book_pref'''
-#    mat[user_keys[rating['user']]][book_keys[rating['isbn']]] = rating['rating'] - float(user['total']) / user['count']
+    mat[user_keys[rating['user']]][book_keys[rating['isbn']]] = rating['rating'] - float(user['total']) / user['count'] + mean_rating - book_pref
+    mat[user_keys[rating['user']]][book_keys[rating['isbn']]] = rating['rating'] - float(user['total']) / user['count']'''
     mat[user_keys[rating['user']]][book_keys[rating['isbn']]] = rating['rating'] - float(user['total']) / user['count']
 
-# kmeans_plus.kmeans_plus(mat,2)
+# TODO number of clusters
+[mu,resp] = kmeans_plus.kmeans_plus(mat,2)
 
 # Make predictions for each test query.
 for query in test_queries:
