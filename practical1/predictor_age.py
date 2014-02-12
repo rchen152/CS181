4cd5d91963de9ff6a6c689204cf2d007e7e2c5bd
Rebecca Chen <rchen@college.harvard.edu>
2014-02-12 11:57:13 -0500

    cluster by age

--- practical1/predictor_age.py
+++ practical1/predictor_age.py
@@ -2,13 +2,13 @@ import numpy as np
 import util
 from sklearn.cluster import Ward
 import kmeans_plus
+import random
+import math
 
 # CRITICAL_BOOK_NUM = 4
 # TODO change this value depending on what your system can handle
 BOOK_MEMORY_ERROR = 7
-NUM_CLUSTERS = 10
-
-def 
+NUM_CLUSTERS = 8
 
 # This makes predictions based on the mean rating for each book in the
 # training data.  When there are no training data for a book, it
@@ -25,43 +25,75 @@ test_queries   = util.load_test(test_filename)
 book_list      = util.load_books(book_filename)
 user_list      = util.load_users(user_filename)
 
+train_data = []
+test_data = []
+
+for datum in training_data:
+    if (random.randrange(2) == 0):
+        train_data.append(datum)
+    else:
+        test_data.append(datum)
+
 # Compute the global mean rating for a fallback.
-num_train = len(training_data)
-mean_rating = float(sum(map(lambda x: x['rating'], training_data)))/num_train
-
-# Turn the list of books into an ISBN-keyed dictionary.
-# Store data for each book to keep track of the per-book average.
-books = {}
-for book in book_list:
-    books[book['isbn']] = { 'total': 0, # For storing the total of ratings.
-                            'count': 0, # For storing the number of ratings.
-                            }
-
-# Iterate over the training data to compute means.
-for rating in training_data:
-    books[rating['isbn']]['total'] += rating['rating']
-    books[rating['isbn']]['count'] += 1
+num_train = len(train_data)
+mean_rating = float(sum(map(lambda x: x['rating'], train_data)))/num_train
 
 num_users = len(user_list)
-mat = np.zeros((num_users, 1))
-
-# Turn the list of users into a dictionary.
-# Store data for each user to keep track of the per-user average.
-users = {}
+mat = np.zeros((num_users,1))
 for i in range(num_users):
-    users[user_list[i]['user']] = {'total': 0, 'count': 0}
     mat[i][0] = user_list[i]['age']
+
+[_,resp] = kmeans_plus.kmeans_plus(mat, NUM_CLUSTERS);
+
+users = {}
+clusters = []
+for i in range(NUM_CLUSTERS):
+    clusters.append(set())
+for i in range(num_users):
+    clusters[resp[i]].add(user_list[i]['user'])
+    users[user_list[i]['user']] = resp[i]
+
+index = 0
+b_keys = {}
+for book in book_list:
+    b_keys[book['isbn']] = index
+    index += 1
+
+train_sorted = []
+num_books = len(book_list)
+for i in range(num_books):
+    train_sorted.append(set())
+
+for rating in train_data:
+    train_sorted[b_keys[rating['isbn']]].add((rating['user'],rating['rating']))
     
-# Iterate over the training data to compute means.
-for rating in training_data:
-    user_id = rating['user']
-    users[user_id]['total'] += rating['rating']
-    users[user_id]['count'] += 1
+sum_errors = 0.
+for datum in test_data:
+    cluster = clusters[users[datum['user']]]
+    sum_ratings = 0.
+    num_ratings = 0
+    for (u,r) in train_sorted[b_keys[datum['isbn']]]:
+        if (u in cluster):
+            sum_ratings += r
+            num_ratings += 1
+    if (num_ratings == 0):
+        sum_errors += math.pow(mean_rating - datum['rating'],2)
+    else:
+        sum_errors += math.pow((sum_ratings / num_ratings) - datum['rating'],2)
+print math.sqrt(sum_errors / len(test_data))
 
-[mu,resp] = kmeans_plus.kmeans_plus(mat, NUM_CLUSTERS)
-
-for query in test_queries:
-    query['rating'] = mean_rating
+'''for query in test_queries:
+    cluster = clusters[users[query['user']]]
+    sum_ratings = 0.
+    num_ratings = 0
+    for (u,r) in train_sorted[b_keys[query['isbn']]]:
+        if (u in cluster):
+            sum_ratings += r
+            num_ratings += 1
+    if (num_ratings == 0):
+        query['rating'] = mean_rating
+    else:
+        query['rating'] = sum_ratings / num_ratings
 
 # Write the prediction file.
-util.write_predictions(test_queries, pred_filename)
+util.write_predictions(test_queries, pred_filename)'''
--- practical1/predictor_half.py
+++ practical1/predictor_half.py
@@ -1,19 +1,20 @@
 import numpy as np
 import util
-from sklearn.decomposition import PCA
+from sklearn.cluster import Ward
 import kmeans_plus
+import random
+import math
 
 # CRITICAL_BOOK_NUM = 4
 # TODO change this value depending on what your system can handle
-NUM_COMPONENTS = 300
-BOOK_MEMORY_ERROR = 6
+BOOK_MEMORY_ERROR = 8
 NUM_CLUSTERS = 5
 
 # This makes predictions based on the mean rating for each book in the
 # training data.  When there are no training data for a book, it
 # defaults to the global mean.
 
-pred_filename  = 'pca_corr_metric2.csv'
+pred_filename  = 'predictor-kmeans10.csv'
 train_filename = 'ratings-train.csv'
 test_filename  = 'ratings-test.csv'
 book_filename  = 'books.csv'
@@ -24,9 +25,18 @@ test_queries   = util.load_test(test_filename)
 book_list      = util.load_books(book_filename)
 user_list      = util.load_users(user_filename)
 
+train_data = []
+test_data = []
+
+for datum in training_data:
+    if (random.randrange(2) == 0):
+        train_data.append(datum)
+    else:
+        test_data.append(datum)
+
 # Compute the global mean rating for a fallback.
-num_train = len(training_data)
-mean_rating = float(sum(map(lambda x: x['rating'], training_data)))/num_train
+num_train = len(train_data)
+mean_rating = float(sum(map(lambda x: x['rating'], train_data)))/num_train
 
 # Turn the list of books into an ISBN-keyed dictionary.
 # Store data for each book to keep track of the per-book average.
@@ -37,13 +47,13 @@ for book in book_list:
                             }
 
 # Iterate over the training data to compute means.
-for rating in training_data:
+for rating in train_data:
     books[rating['isbn']]['total'] += rating['rating']
     books[rating['isbn']]['count'] += 1
 
 book_short = [book for book in book_list if books[book['isbn']]['count'] > BOOK_MEMORY_ERROR]
 
-train_short = [rating for rating in training_data if books[rating['isbn']]['count'] > BOOK_MEMORY_ERROR]
+train_short = [rating for rating in train_data if books[rating['isbn']]['count'] > BOOK_MEMORY_ERROR]
 
 # Turn the list of users into a dictionary.
 # Store data for each user to keep track of the per-user average.
@@ -93,14 +103,7 @@ for rating in train_short:
 
     mat[user_keys[rating['user']]][book_keys[rating['isbn']]] = rating['rating'] - float(user['total']) / user['count']
 
-pca = PCA(n_components = NUM_COMPONENTS)
-reduced_mat = pca.fit_transform(mat)
-vars = pca.explained_variance_ratio_
-x = [w for w in range(NUM_COMPONENTS)]
-plt.plot(x,y,'ro')
-plt.savefig('pca_variances.png')
-
-[mu,resp] = kmeans_plus.kmeans_plus(reduced_mat, NUM_CLUSTERS)
+[mu,resp] = kmeans_plus.kmeans_plus(mat, NUM_CLUSTERS)
 
 cluster_ids = []
 for i in range(NUM_CLUSTERS):
@@ -119,13 +122,14 @@ training_sorted = []
 for i in range(len(book_list)):
     training_sorted.append(set())
 
-for rating in training_data:
+for rating in train_data:
     training_sorted[long_book_keys[rating['isbn']]].add((rating['user'],rating['rating']))
 
-for query in test_queries:
+sum_errors = 0.
+for query in test_data:
     user = users[query['user']]
     # TODO global mean rating or mean rating on book?
-    if(query['user'] in user_keys):
+    if (query['user'] in user_keys):
         cluster = cluster_ids[resp[user_keys[query['user']]]]
         sum_zetas = 0
         num_zetas = 0
@@ -134,16 +138,15 @@ for query in test_queries:
                 sum_zetas += rate - float(users[use]['total'])/users[use]['count']
                 num_zetas += 1
         if (num_zetas == 0):
-            query['rating'] = mean_rating
+            sum_errors += math.pow(mean_rating - query['rating'],2)
         else:
             temp_rating = float(sum_zetas)/num_zetas + float(user['total'])/user['count']
             if(temp_rating > 5):
                 temp_rating = 5
             if(temp_rating < 1):
                 temp_rating = 1
-            query['rating'] = temp_rating
+            sum_errors += math.pow(temp_rating - query['rating'],2)
     else:
-        query['rating'] = mean_rating
+        sum_errors += math.pow(mean_rating - query['rating'],2)
 
-# Write the prediction file.
-util.write_predictions(test_queries, pred_filename)
+print math.sqrt(sum_errors / len(test_data))
