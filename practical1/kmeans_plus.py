3710858b50baab5b98c269c08f99126e71f890f4
Rebecca Chen <rchen@college.harvard.edu>
2014-02-14 18:06:38 -0500

    shouldn't be any changes, but pushing just to make sure

--- practical1/kmeans_plus.py
+++ practical1/kmeans_plus.py
@@ -43,7 +43,7 @@ def kmeans_plus(m, k):
     num_pts = mat.shape[0]
     dim = mat.shape[1]
     mu = np.zeros((k,dim))
-    mu[0] = mat[random.randrange(num_pts)]
+    '''mu[0] = mat[random.randrange(num_pts)]
     diff = mat - np.ones((num_pts, dim))*mu[0]
     dist = np.sum(diff*diff,axis = 1).reshape(num_pts,1)
     normalized = (np.transpose(dist/np.sum(dist,axis = 0)))[0]
@@ -53,17 +53,26 @@ def kmeans_plus(m, k):
         temp_diff = mat - np.ones((num_pts, dim))*mu[i]
         temp_dist = np.sum(temp_diff*temp_diff,axis = 1).reshape(num_pts,1)
         dist = np.min(np.concatenate((dist,temp_dist), axis = 1), axis =1).reshape(num_pts,1)
-        normalized = (np.transpose(dist/np.sum(dist,axis = 0)))[0]
+        normalized = (np.transpose(dist/np.sum(dist,axis = 0)))[0]'''
     resp = np.zeros((num_pts,1))
+
+    #normal k_means
+    for i in range(num_pts):
+        resp[i] = random.randrange(k)
+
     result = np.zeros((num_pts,k))
     while(True):    
+        #normal k_means
+        for i in range(k):
+    	    mu[i] = np.mean(mat[resp[:,0]==i,:],axis=0)
+
         result = kmeans_metric_corr(mat, mu)
 	temp_resp = np.argmin(result,axis = 1).reshape(num_pts,1)
 	if(temp_resp == resp).all():
 	     break
         resp = temp_resp
-        for i in range(k):
-    	    mu[i] = np.mean(mat[resp[:,0]==i,:],axis=0)
+        '''for i in range(k):
+    	    mu[i] = np.mean(mat[resp[:,0]==i,:],axis=0)'''
     error = math.sqrt(sum(np.min(result,axis = 1))/num_pts)
     print error
     return np.array([mu,resp])
