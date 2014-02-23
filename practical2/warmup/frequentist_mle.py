964caa67c300e18dc8c7abc599d554820523095a
Aaron Landesman <aaronlandesman@gmail.com>
2014-02-23 12:11:23 -0500

    updated frequentist

--- practical2/warmup/frequentist_mle.py
+++ practical2/warmup/frequentist_mle.py
@@ -14,7 +14,10 @@ flt_data = map(lambda x: map(float,x),arr)
 num_data = len(flt_data)
 data_array = np.array(flt_data)
 
-basis_fns  = [lambda x: 1, lambda x : x, lambda x: x**2, lambda x: x**3, lambda x : x**4]
+basis_fns  = [lambda x: 1, lambda x: x,lambda x : x**2, lambda x: x**3, lambda x : x**4,lambda x: x**5, lambda x: x**6,lambda x : x**7, lambda x: x**8, lambda x : x**9]
+
+
+'''basis_fns  = [lambda x: 1, lambda x: np.sin(.1*x),lambda x : np.sin(.20*x), lambda x: np.sin(.30*x), lambda x : np.sin(.40*x)]'''
 
 fst_fn = basis_fns[0]
 
@@ -28,17 +31,25 @@ pseudo_inv = np.linalg.pinv(phi)
 coeffs = np.dot(pseudo_inv, data_array[:,1])
 print coeffs
 
+var_vect = data_array[:,1] - np.dot(phi, np.transpose(coeffs))
+mle_variance = np.sum(var_vect * var_vect,axis = 0)/num_data
+mle_std = np.sqrt(mle_variance)
+print mle_std
+
 def reg(x):
     out = 0.
     for i in range(len(basis_fns)):
         out += coeffs[i]*basis_fns[i](x)
     return out
-print coeffs[0]
 
-x = np.linspace(0, 60)
+x = np.linspace(0, 60,1000)
 y = reg(x)
+w = map(lambda t: t + mle_std,y)
+z = map(lambda t: t - mle_std,y)
 
 plt.plot(x,y)
+plt.plot(x,w)
+plt.plot(x,z)
 
 plt.plot(data_array[:,0],data_array[:,1],'ro')
 plt.show()
