e07d93a0431ef41e42226a538b59f0f24e229a03
Aaron Landesman <aaronlandesman@gmail.com>
2014-02-25 20:44:43 -0500

    edited regression starter

--- practical2/regression_starter.py
+++ practical2/regression_starter.py
@@ -70,6 +70,164 @@ def extract_feats(ffs, datafile="train.xml", global_feat_dict=None):
       a sparse design matrix, a dict mapping features to column-numbers,
       a vector of target values, and a list of movie ids in order of their
       rows in the design matrix
+    """
+    fds = [] # list of feature dicts
+    targets = []
+    ids = [] 
+    begin_tag = "<instance" # for finding instances in the xml file
+    end_tag = "</instance>"
+    in_instance = False
+    curr_inst = [] # holds lines of file associated with current instance
+
+    # start iterating thru file
+    with open(datafile) as f:
+        # get rid of first two lines
+        _ = f.readline()
+        _ = f.readline()
+        for line in f:
+            if begin_tag in line:
+                if in_instance: 
+                    assert False  # cannot have nested instances
+                else:
+                    curr_inst = [line]
+                    in_instance = True
+            elif end_tag in line:
+                # we've read in an entire instance; we can extract features
+                curr_inst.append(line)
+                # concatenate the lines we've read and parse as an xml element
+                movie_data = util.MovieData(ET.fromstring("".join(curr_inst)))
+                rowfd = {}
+                # union the output of all the feature functions over this instance
+                [rowfd.update(ff(movie_data)) for ff in ffs]
+                # add the final dictionary for this instance to our list
+                fds.append(rowfd)
+                # add target val
+                targets.append(movie_data.target)
+                # keep track of the movie id's for later
+                ids.append(movie_data.id)
+                # reset
+                curr_inst = []
+                in_instance = False
+            elif in_instance:
+                curr_inst.append(line)
+
+    X,feat_dict = make_design_mat(fds,global_feat_dict)
+    return X, feat_dict, np.array(targets), ids
+
+
+def make_design_mat(fds, global_feat_dict=None):
+    """
+    arguments:
+      fds is a list of feature dicts (one for each row).
+      global_feat_dict is a dictionary mapping feature_names to column-numbers; it
+      should only be provided when extracting features from test data, so that 
+      the columns of the test matrix align correctly.
+       
+    returns: 
+        a sparse NxD design matrix, where N == len(fds) and D is the number of
+        the union of features defined in any of the fds 
+    """
+    if global_feat_dict is None:
+        all_feats = set()
+        [all_feats.update(fd.keys()) for fd in fds]
+        feat_dict = dict([(feat, i) for i, feat in enumerate(sorted(all_feats))])
+    else:
+        feat_dict = global_feat_dict
+        
+    cols = []
+    rows = []
+    data = []        
+    for i in xrange(len(fds)):
+        temp_cols = []
+        temp_data = []
+        for feat,val in fds[i].iteritems():
+            try:
+                # update temp_cols iff update temp_data
+                temp_cols.append(feat_dict[feat])
+                temp_data.append(val)
+            except KeyError as ex:
+                if global_feat_dict is not None:
+                    pass  # new feature in test data; nbd
+                else:
+                    raise ex
+
+        # all fd's features in the same row
+        k = len(temp_cols)
+        cols.extend(temp_cols)
+        data.extend(temp_data)
+        rows.extend([i]*k)
+
+    assert len(cols) == len(rows) and len(rows) == len(data)
+   
+
+    X = sparse.csr_matrix((np.array(data),
+                   (np.array(rows), np.array(cols))),
+                   shape=(len(fds), len(feat_dict)))
+    return X, feat_dict
+    
+
+## Here are two example feature-functions. They each take in a util.MovieData
+## object, and return a dictionary mapping feature-names to numeric values.
+## TODO: modify these functions, and/or add new ones.
+def metadata_feats(md):
+    """
+    arguments:
+      md is a util.MovieData object
+    returns:
+      a dictionary containing a mapping from a subset of the possible metadata features
+      to their values on this util.MovieData object
+    """
+    d = {}
+    for k,v in md.__dict__.iteritems():
+        if k in util.MovieData.implicit_list_atts or k in util.MovieData.reviewers:
+            continue
+        if k == "target":
+            continue
+        if isinstance(v, list):
+            d.update([(k+"-"+val,1) for val in v])
+        elif isinstance(v, float):
+            d[k] = v
+        elif isinstance(v, bool):
+            d[k] = float(v)
+        else:
+            d[k+"-"+v] = 1
+    return d
+
+def unigram_feats(md):
+    """
+    arguments:
+      md is a util.MovieData object
+    returns:
+      a dictionary containing a mapping from unigram features from the reviews
+      to their values on this util.MovieData object
+    """
+    c = Counter()
+    for rev in util.MovieData.reviewers:
+        if hasattr(md,rev):
+            # count occurrences of asciified, lowercase, non-numeric unigrams 
+            # after removing punctuation
+            c.update([token for token in 
+                        util.punct_patt.sub("",
+                         util.asciify(md.__dict__[rev].strip().lower())).split()
+                          if util.non_numeric(token)])
+    return c
+
+
+## The following function does the feature extraction, learning, and prediction
+def main():
+    trainfile = "train.xml"
+    testfile = "testcases.xml"
+    outputfile = "mypredictions.csv"  # feel free to change this or take it as an argument
+    
+    # TODO put the names of the feature functions you've defined above in this list
+    ffs = [metadata_feats, unigram_feats]
+    
+    # extract features
+    print "extracting training features..."
+    X_train,global_feat_dict,y_train,train_ids = extract_feats(ffs, trainfile)
+    print "done extracting training features"
+    print
+    
     # TODO train here, and return regression parameters
     print "learning..."
     learned_w = splinalg.lsqr(X_train,y_train)[0]
