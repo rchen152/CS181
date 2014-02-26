369011a2575914b85ec66eb78377350fd971944a
Rebecca Chen <rchen@college.harvard.edu>
2014-02-26 18:30:32 -0500

    more stuffs

--- practical2/correlation.py
+++ practical2/correlation.py
@@ -7,14 +7,19 @@ try:
 except ImportError:
     import xml.etree.ElementTree as ET
 import matplotlib.pyplot as pp
+import heapq as heap
+import regression_starter as rs
 
 CONT_FEATS = ['running_time','number_of_screens','production_budget',
               'num_highest_grossing_actors','num_oscar_winning_actors']
 
-#BOOL_FEATS = ['summer_release', 'christmas_release', 'memorial_release',
-              #'independence_release', 'labor_release']
-#BOOL_FEATS = ['rating']
-BOOL_FEATS = ['highest_grossing_actors_present', 'oscar_winning_directors_present', 'oscar_winning_actors_present']
+BOOL_FEATS = ['rating','highest_grossing_actors_present',
+              'oscar_winning_directors_present', 'oscar_winning_actors_present',
+              'summer_release', 'christmas_release', 'memorial_release',
+              'independence_release', 'labor_release']
+
+MOVIE_TARGET = 10
+WORD_TARGET = 10
 
 def correlate(lst1, lst2):
     return np.corrcoef(np.array(lst1), np.array(lst2))[0][1]
@@ -22,7 +27,7 @@ def correlate(lst1, lst2):
 def comp_p(lst1, lst2):
     return stats.ks_2samp(np.array(lst1), np.array(lst2))
 
-def get_part(k,v):
+def part(k,v):
     if k == 'rating':
         return v == 'R'
     elif 'present' in k:
@@ -30,21 +35,16 @@ def get_part(k,v):
     else:
         return v
 
-def extract_cont_feats(feats, data_file):
+def extract_gen_feats(feats, data_file, all_feats, build, update, postproc):
     good_feats = []
     for feat in feats:
-        if feat in CONT_FEATS:
+        if feat in all_feats:
             good_feats.append(feat)
         else:
             print (feat + " is not a supported feature")
     feats = good_feats
 
-    feat_dict = {}
-    num_feats = len(feats)
-    for i in range(num_feats):
-        feat_dict[feats[i]] = {'total':0., 'count':0., 'vec':[]}
-
-    reg_dict = {'total':0., 'count':0., 'vec':[]}
+    feat_dict = build(feats)
     curr_inst = []
 
     begin_tag = "<instance" # for finding instances in the xml file
@@ -64,92 +64,96 @@ def extract_cont_feats(feats, data_file):
             elif end_tag in line:
                 curr_inst.append(line)
                 md = util.MovieData(ET.fromstring("".join(curr_inst)))
-                for k,v in md.__dict__.iteritems():
-                    for i in range(num_feats):
-                        if feats[i] == k:
-                            feat_dict[feats[i]]['total'] += v
-                            feat_dict[feats[i]]['count'] += 1
-                            feat_dict[feats[i]]['vec'].append(v)
-                reg_dict['total'] += md.target
-                reg_dict['count'] += 1
-                reg_dict['vec'].append(md.target)
-                for i in range(num_feats):
-                    if len(feat_dict[feats[i]]['vec']) < reg_dict['count']:
-                        feat_dict[feats[i]]['vec'].append(0.)
+                feat_dict = update(md, feats, feat_dict)
                 curr_inst = []
                 in_instance = False
             elif in_instance:
                 curr_inst.append(line)
 
-    feat_dict['regy'] = reg_dict
-    num_feats += 1
-    feat_out = {}
-    for k in feat_dict:
-        tot = feat_dict[k]['total']
-        cnt = feat_dict[k]['count']
-        zvec = [i - (tot/cnt) for i in feat_dict[k]['vec']]
-        feat_out[k] = {'total':tot, 'count':cnt, 'vec': zvec}
-    return feat_out
-
-def partition_bool_feats(feats, data_file):
-    good_feats = []
-    for feat in feats:
-        if feat in BOOL_FEATS:
-            good_feats.append(feat)
-        else:
-            print (feat + " is not a supported feature")
-    feats = good_feats
-
-    feat_dict = {}
-    num_feats = len(feats)
-    for i in range(num_feats):
-        feat_dict[feats[i]] = {'true':{'tot':0.,'lg_tot':0.,'count':0,'vec':[]},
-                               'false':{'tot':0.,'lg_tot':0.,'count':0,'vec':[]}}
-    flags = ['false','false','false']
-    curr_inst = []
-
-    begin_tag = "<instance" # for finding instances in the xml file
-    end_tag = "</instance>"
-    in_instance = False
-
-    with open(data_file) as f:
-        f.readline()
-        f.readline()
-        for line in f:
-            if begin_tag in line:
-                if in_instance:
-                    assert False
-                else:
-                    curr_inst = [line]
-                    in_instance = True
-            elif end_tag in line:
-                curr_inst.append(line)
-                md = util.MovieData(ET.fromstring("".join(curr_inst)))
-                flags = ['false','false','false']
-                for k,v in md.__dict__.iteritems():
-                    for i in range(num_feats):
-                        if feats[i] == k:
-                            flags[i] = 'true'
-                            #part = 'false'
-                            #if v == 'R':
-                                #part = 'true'
-                            #feat_dict[k][part]['vec'].append(md.target)
-                            #feat_dict[k][part]['tot'] += md.target
-                            #feat_dict[k][part]['lg_tot'] += math.log(md.target)
-                            #feat_dict[k][part]['count'] += 1
-                for i in range(3):
-                    feat_dict[feats[i]][flags[i]]['vec'].append(md.target)
-                    feat_dict[feats[i]][flags[i]]['tot'] += md.target
-                    feat_dict[feats[i]][flags[i]]['lg_tot']+=math.log(md.target)
-                    feat_dict[feats[i]][flags[i]]['count'] += 1
-                curr_inst = []
-                in_instance = False
-            elif in_instance:
-                curr_inst.append(line)
-
+    feat_dict = postproc(feat_dict)
     return feat_dict
 
-feat_dict = partition_bool_feats(BOOL_FEATS, 'train.xml')
+def extract_cont_feats(feats, data_file):
+    def build(feat_lst):
+        feat_dict = {}
+        for k in feat_lst:
+            feat_dict[k] = {'total':0., 'count':0., 'vec':[]}
+        feat_dict['regy'] = {'total':0., 'count':0., 'vec':[]}
+        return feat_dict
+    def update(md, feat_lst, feat_dict):
+        for k,v in md.__dict__.iteritems():
+            if k in feat_lst:
+                feat_dict[k]['total'] += v
+                feat_dict[k]['count'] += 1
+                feat_dict[k]['vec'].append(v)
+        feat_dict['regy']['total'] += md.target
+        feat_dict['regy']['count'] += 1
+        feat_dict['regy']['vec'].append(md.target)
+        for k in feat_lst:
+            if len(feat_dict[k]['vec']) < feat_dict['regy']['count']:
+                feat_dict[k]['vec'].append(0.)
+        return feat_dict
+    def postproc(feat_dict):
+        feat_out = {}
+        for k in feat_dict:
+            tot = feat_dict[k]['total']
+            cnt = feat_dict[k]['count']
+            zvec = [i - (tot/cnt) for i in feat_dict[k]['vec']]
+            feat_out[k] = {'total':tot, 'count':cnt, 'vec': zvec}
+        return feat_out
+    return extract_gen_feats(feats, data_file, CONT_FEATS,
+                             build, update, postproc)
+
+def extract_bool_feats(feats, data_file):
+    def build(feat_lst):
+        feat_dict = {}
+        for k in feat_lst:
+            feat_dict[k] = {'sat'  :False,
+                            'true' :
+                            {'tot':0.,'lg_tot':0.,'count':0,'vec':[]},
+                            'false':
+                            {'tot':0.,'lg_tot':0.,'count':0,'vec':[]}}
+        return feat_dict
+    def update(md, feat_lst, feat_dict):
+        for k in feat_lst:
+            feat_dict[k]['sat'] = False
+        for k,v in md.__dict__.iteritems():
+            if k in feat_lst:
+                feat_dict[k]['sat'] = part(k,v)
+        for k in feat_lst:
+            sat = str(feat_dict[k]['sat']).lower()
+            feat_dict[k][sat]['vec'].append(md.target)
+            feat_dict[k][sat]['tot'] += md.target
+            feat_dict[k][sat]['lg_tot']+=math.log(md.target)
+            feat_dict[k][sat]['count'] += 1
+        return feat_dict
+    def postproc(feat_dict):
+        for k in feat_dict:
+            feat_dict[k].pop('sat')
+        return feat_dict
+    return extract_gen_feats(feats, data_file, BOOL_FEATS,
+                             build, update, postproc)
+
+def extract_reviews(data_file):
+    def build(feat_lst):
+        return []
+    def update(md, feat_lst, feat_dict):
+        text = rs.unigram_feats(md)
+        text['regy'] = md.target
+        feat_dict.append(text)
+        return feat_dict
+    def postproc(feat_dict):
+        return rs.make_design_mat(feat_dict)
+    return extract_gen_feats([], data_file, [], build, update, postproc)
+
+'''feat_dict = extract_cont_feats(['production_budget','ohaiyo'], 'train.xml')
+for k in feat_dict:
+    print k
+    print feat_dict[k]['count']
+    print correlate(feat_dict[k]['vec'], feat_dict['regy']['vec'])
+    print '--------'
+
+feat_dict = extract_bool_feats(['fizzbuzz', 'rating'], 'train.xml')
 for k in feat_dict:
     true = feat_dict[k]['true']
     false = feat_dict[k]['false']
@@ -160,10 +164,40 @@ for k in feat_dict:
     print 'log mean diff ' + str(lg_diff)
     print 'true  ' + str(len(true['vec']))
     print 'false ' + str(len(false['vec']))
-    print comp_p(true['vec'], false['vec'])
-    print '--------'
+    print comp_p(true['vec'], false['vec'])'''
+#    print '--------'
 
-    pp.plot(true['vec'], 'ro')
-    pp.plot(false['vec'], 'bo')
-    pp.show()
-    pp.clf()
+'''mat, key = extract_reviews('train.xml')
+
+num_movies,_ = mat.get_shape()
+xs = []
+ys = []
+for i in range(num_movies):
+    regy = mat[i,key['regy']]
+    mat[i,key['regy']] = 0
+    xs.append(mat[i].sum())
+    ys.append(math.log(regy))
+pp.plot(xs,ys,'ro')
+pp.show()'''
+
+'''inv_key = {v:k for k,v in key.items()}
+num_movies,num_words = mat.get_shape()
+min_movies = []
+max_movies = []
+
+for i in range(num_movies):
+    regy = mat[i,key['regy']]
+    if len(min_movies) < MOVIE_TARGET:
+        heap.heappush(min_movies, (-1.*regy,i))
+    else:
+        heap.heappushpop(min_movies, (-1.*regy,i))
+    if len(max_movies) < MOVIE_TARGET:
+        heap.heappush(max_movies, (regy,i))
+    else:
+        heap.heappushpop(max_movies, (regy,i))
+
+words = np.zeros((num_words))
+for mv in min_movies:
+    words += -1*mat[mv[1]]
+for mv in max_movies:
+    words += mat[mv[1]]'''
---
+++ practical2/prelim.py
@@ -0,0 +1,72 @@
+import heapq as heap
+import math
+import matplotlib.pyplot as pp
+import numpy as np
+import regression_starter as rs
+import scipy.stats as stats
+
+CONT_FEATS = ['running_time','number_of_screens','production_budget',
+              'num_highest_grossing_actors','num_oscar_winning_actors']
+
+BOOL_FEATS = ['rating-R','highest_grossing_actors_present',
+              'oscar_winning_directors_present', 'oscar_winning_actors_present',
+              'summer_release', 'christmas_release', 'memorial_release',
+              'independence_release', 'labor_release']
+
+MOVIE_TARGET = 100
+WORD_TARGET = 100
+
+def proc_metadata_feats():
+    mat,key,regy,_ = rs.extract_feats([rs.metadata_feats])
+    num_movies = len(regy)
+
+    for feat in CONT_FEATS:
+        print feat
+        print np.corrcoef(mat.getcol(key[feat]).todense().transpose(),regy)[0,1]
+        print '-------------'
+
+    for feat in BOOL_FEATS:
+        feat_arr = mat.getcol(key[feat]).todense().transpose()
+        tvec = [regy[i] for i in range(len(regy)) if feat_arr[0,i] == 1.]
+        fvec = [regy[i] for i in range(len(regy)) if feat_arr[0,i] == 0.]
+        tlen = len(tvec)
+        flen = len(fvec)
+
+        print feat + ' ' + str(tlen) + '/' + str(flen)
+        print 'mean diff ' + str((sum(tvec)/tlen) - (sum(fvec)/flen))
+        print 'log mean diff ' + str((sum([math.log(r) for r in tvec])/tlen) -
+                                     (sum([math.log(r) for r in fvec])/flen))
+        print stats.ks_2samp(np.array(tvec),np.array(fvec))[0]
+        print '-------------'
+
+def proc_unigram_feats():
+    mat,key,regy,_ = rs.extract_feats([rs.unigram_feats])
+    inv_key = {v:k for k,v in key.items()}
+    num_movies,num_words = mat.get_shape()
+
+    movies = [(regy[i],i) for i in range(num_movies)]
+    min_movies = heap.nsmallest(MOVIE_TARGET,movies)
+    max_movies = heap.nlargest(MOVIE_TARGET,movies)
+    tot_min = 0.
+    tot_max = 0.
+    for mv in min_movies:
+        tot_min += mat[mv[1]].sum()
+    for mv in max_movies:
+        tot_max += mat[mv[1]].sum()
+    fix = tot_max/tot_min
+    diffs = np.zeros((num_words))
+    for mv in min_movies:
+        diffs += -1.*fix*mat[mv[1]]
+    for mv in max_movies:
+        diffs += mat[mv[1]]
+    words = [(diffs[0,i],inv_key[i]) for i in range(num_words)]
+    worst_words = heap.nsmallest(WORD_TARGET, words)
+    best_words = heap.nlargest(WORD_TARGET, words)
+
+    for wd in worst_words:
+        print wd[1]
+    print '---------------------------------'
+    for wd in best_words:
+        print wd[1]
+
+proc_unigram_feats()
