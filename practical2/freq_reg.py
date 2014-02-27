8d86a00ea4e49c6fb2e931c966c8ee9894195b30
Rebecca Chen <rchen@college.harvard.edu>
2014-02-27 17:13:57 -0500

    new code

--- practical2/freq_reg.py
+++ practical2/freq_reg.py
@@ -15,46 +15,13 @@ def coeffs(basis_fns, train_data, LAMBDA = 0):
     coeffs = np.dot(pseudo_inv, train_data[:,1])
     return coeffs
 
+def product(x, coeffs, basis_fns):
+    out = 0.
+    for i in range(len(basis_fns)):
+        out += coeffs[i]*basis_fns[i](x)
+    return out
+
 def regress(basis_fns, train_data, test_data, LAMBDA = 0):
-
-    fst_fn = basis_fns[0]
-
-    trans = [map(fst_fn, train_data)]
-
-    num_pts = len(train_data[:,0])
-
-    for fn in (basis_fns[1:]):
-        trans = np.concatenate((trans,[map(fn, train_data[:,0])]),axis = 0)
-    phi = np.transpose(trans)
-
-    temp_pseudo = LAMBDA * np.identity(len(basis_fns))+np.dot(trans, phi)
-    pseudo_inv = np.dot(np.linalg.inv(temp_pseudo),trans)
-
-    coeffs = np.dot(pseudo_inv, train_data[:,1])
-
-    var_vect = train_data[:,1] - np.dot(phi, np.transpose(coeffs))
-    mle_variance = np.sum(var_vect * var_vect,axis = 0)/num_pts
-    mle_std = np.sqrt(mle_variance)
-
-    def reg(x):
-        out = 0.
-        for i in range(len(basis_fns)):
-            out += coeffs[i]*basis_fns[i](x)
-        return out
-
-    return [reg(test_data[i,0]) for i in range(test_data.shape[0])]
-'''x = np.linspace(0, 60,1000)
-y = reg(x)
-w = map(lambda t: t + mle_std,y)
-z = map(lambda t: t - mle_std,y)
-
-plt.plot(x,y)
-plt.plot(x,w)
-plt.plot(x,z)
-
-plt.plot(data[:,0],data[:,1],'ro')
-plt.title('Frequentist Polynomial Basis, LAMBDA = 10')
-plt.xlabel('time since impact (ms)')
-plt.ylabel('gforce')
-
-plt.savefig('freq_polys.png')'''
+    cs = coeffs(basis_fns, train_data, LAMBDA)
+    return [product(test_data[i,0], cs, basis_fns)
+            for i in range(test_data.shape[0])]
--- practical2/regression.py
+++ practical2/regression.py
@@ -1,295 +1,134 @@
-import freq_reg as reg
+import freq_reg as freg
 import math
 import numpy as np
 import matplotlib.pyplot as plt
 import regression_starter as rs
 import util
 
-def screens_only_double_log():
-    train_mat,key,train_regy,_ = rs.extract_feats([rs.metadata_feats])
-    train_screens = train_mat.getcol(key['number_of_screens']).todense()
-    train_len = len(train_screens)
+def format_arr(feat_cols, regy_vec, fns, check = lambda _ : True):
+    num_data = len(regy_vec)
+    num_feats = len(feat_cols)
+    lst = []
+    for i in range(num_data):
+        datum = [feat_cols[j][i,0] for j in range(num_feats)]
+        if check(datum):
+            lst.append([tuple([fns[j](datum[j]) for j in range(num_feats)]),
+                        fns[num_feats](regy_vec[i])])
+    return np.array(lst)
 
-    train_array = np.zeros((train_len,2))
-    train_array[:,0] = train_screens.reshape((train_len))
-    train_array[:,1] = train_regy.transpose()
-
-    for i in range(train_len):
-        train_array[i,0] = math.log(train_array[i,0])
-        train_array[i,1] = math.log(train_array[i,1])
-
-    test_mat,_,_,test_ids = rs.extract_feats([rs.metadata_feats],
-                                             'testcases.xml',
-                                             global_feat_dict = key)
-    test_screens = test_mat.getcol(key['number_of_screens']).todense()
-    test_len = test_screens.shape[0]
-    for i in range(test_len):
-        test_screens[i,0] = math.log(test_screens[i,0])
-
-    preds = reg.regress([lambda x:1, lambda x:x, lambda x:x**2],
-                        train_array, test_screens)
-
-    pred_lst = [0 if preds[i] < 0 else math.e**preds[i]
-                for i in range(test_len)]
-
-    '''measure = 0.
-    for i in range(test_len):
-        measure += math.fabs(pred_lst[i] - train_regy[i])
-    print measure/test_len'''
-
-    plt.plot(test_screens,pred_lst,'bo')
-    plt.show()
-
-    util.write_predictions(pred_lst, test_ids, 'screens_double_log.csv')
-
-def screens_only_log():
-    train_mat,key,train_regy,_ = rs.extract_feats([rs.metadata_feats])
-    train_screens = train_mat.getcol(key['number_of_screens']).todense()
-    train_len = len(train_screens)
-
-    train_array = np.zeros((train_len,2))
-    train_array[:,0] = train_screens.reshape((train_len))
-    train_array[:,1] = train_regy.transpose()
-
-    for i in range(train_len):
-        train_array[i,1] = math.log(train_array[i,1])
-
-    test_mat,_,_,test_ids = rs.extract_feats([rs.metadata_feats],
-                                             'testcases.xml',
-                                             global_feat_dict = key)
-    test_screens = test_mat.getcol(key['number_of_screens']).todense()
-    test_len = test_screens.shape[0]
-
-    preds = reg.regress([lambda x:1, lambda x:x, lambda x:x**2],
-                        train_array, test_screens)
-
-    plt.plot(test_screens,preds,'bo')
-    plt.show()
-
-    pred_lst = [0 if preds[i] < 0 else math.e**preds[i]
-                for i in range(test_len)]
-
-    '''measure = 0.
-    for i in range(test_len):
-        measure += math.fabs(pred_lst[i] - train_regy[i])
-    print measure/test_len'''
-
-    util.write_predictions(pred_lst, test_ids, 'screens_log.csv')
-
-def screens_and_budget():
-    train_mat,key,train_regy,_ = rs.extract_feats([rs.metadata_feats])
-    train_screens = train_mat.getcol(key['number_of_screens']).todense()
-    train_budget = train_mat.getcol(key['production_budget']).todense()
-
-    train_len = len(train_screens)
-
-    #train_ind_dict = {}
-    budget = []
-    for i in range(train_len):
-        if train_budget[i,0] > 0.:
-            #train_ind_dict[len(budget)] = i
-            budget.append([(math.log(train_screens[i,0]),
-                            math.log(train_budget[i,0])),
-                           math.log(train_regy[i])])
-    budget_arr = np.array(budget)
-
-    no_budget_arr = np.zeros((train_len,2))
-    no_budget_arr[:,0] = train_screens.reshape((train_len))
-    no_budget_arr[:,1] = train_regy.transpose()
-    for i in range(train_len):
-        no_budget_arr[i,0] = math.log(no_budget_arr[i,0])
-        no_budget_arr[i,1] = math.log(no_budget_arr[i,1])
-    
-    test_mat,_,_,test_ids = rs.extract_feats([rs.metadata_feats],
-                                             'testcases.xml',
-                                             global_feat_dict = key)
-    test_screens = test_mat.getcol(key['number_of_screens']).todense()
-    test_budget = test_mat.getcol(key['production_budget']).todense()
-    test_len = len(test_screens)
-
-    test_ind_dict = {}
-    budget_lst = []
-    no_budget_lst = []
-    for i in range(test_len):
-        if test_budget[i,0] > 0.:
-            test_ind_dict['budget-' + str(len(budget_lst))] = i
-            budget_lst.append([(math.log(test_screens[i,0]),
-                                math.log(test_budget[i,0]))])
-        else:
-            test_ind_dict['none-' + str(len(no_budget_lst))] = i
-            no_budget_lst.append([math.log(test_screens[i,0])])
-    test_budget_arr = np.array(budget_lst)
-    test_no_budget_arr = np.array(no_budget_lst)
-
-    preds_budget = reg.regress([lambda (x,y):1,lambda (x,y):x,lambda (x,y):x**2,
-                                lambda (x,y):y, lambda (x,y):y**2],
-                               budget_arr, test_budget_arr)
-    preds_no_budget = reg.regress([lambda x:1, lambda x:x, lambda x:x**2],
-                                  no_budget_arr, test_no_budget_arr)
-
-    preds = [0 for i in range(test_len)]
-    for i in range(len(preds_budget)):
-        preds[test_ind_dict['budget-'+str(i)]] = preds_budget[i]
-    for i in range(len(preds_no_budget)):
-        preds[test_ind_dict['none-'+str(i)]] = preds_no_budget[i]
-
-    pred_lst = [0 if preds[i] < 0 else math.e**preds[i]
-                for i in range(test_len)]
-
-    '''measure = 0.
-    for i in range(test_len):
-        measure += math.fabs(pred_lst[i] - train_regy[i])
-    print measure/test_len'''
-
-    #plt.plot(test_screens,pred_lst,'bo')
-    #plt.show()
-
-    util.write_predictions(pred_lst, test_ids, 'screens_log_budget_log.csv')
-
-def screens_and_budget():
-    train_mat,key,train_regy,_ = rs.extract_feats([rs.metadata_feats])
-    train_screens = train_mat.getcol(key['number_of_screens']).todense()
-    train_budget = train_mat.getcol(key['production_budget']).todense()
-
-    train_len = len(train_screens)
-
-    budget = []
-    for i in range(train_len):
-        if train_budget[i,0] > 0.:
-            budget.append([(math.log(train_screens[i,0]),
-                            math.log(train_budget[i,0])),
-                           math.log(train_regy[i])])
-    budget_arr = np.array(budget)
-
-    no_budget_arr = np.zeros((train_len,2))
-    no_budget_arr[:,0] = train_screens.reshape((train_len))
-    no_budget_arr[:,1] = train_regy.transpose()
-    for i in range(train_len):
-        no_budget_arr[i,0] = math.log(no_budget_arr[i,0])
-        no_budget_arr[i,1] = math.log(no_budget_arr[i,1])
-    
-    test_mat,_,_,test_ids = rs.extract_feats([rs.metadata_feats],
-                                             'testcases.xml',
-                                             global_feat_dict = key)
-    test_screens = test_mat.getcol(key['number_of_screens']).todense()
-    test_budget = test_mat.getcol(key['production_budget']).todense()
-    test_len = len(test_screens)
-
-    test_ind_dict = {}
-    budget_lst = []
-    no_budget_lst = []
-    for i in range(test_len):
-        if test_budget[i,0] > 0.:
-            test_ind_dict['budget-' + str(len(budget_lst))] = i
-            budget_lst.append([(math.log(test_screens[i,0]),
-                                math.log(test_budget[i,0]))])
-        else:
-            test_ind_dict['none-' + str(len(no_budget_lst))] = i
-            no_budget_lst.append([math.log(test_screens[i,0])])
-    test_budget_arr = np.array(budget_lst)
-    test_no_budget_arr = np.array(no_budget_lst)
-
-    preds_budget = reg.regress([lambda (x,y):1,lambda (x,y):x,lambda (x,y):x**2,
-                                lambda (x,y):y, lambda (x,y):y**2],
-                               budget_arr, test_budget_arr)
-    preds_no_budget = reg.regress([lambda x:1, lambda x:x, lambda x:x**2],
-                                  no_budget_arr, test_no_budget_arr)
-
-    preds = [0 for i in range(test_len)]
-    for i in range(len(preds_budget)):
-        preds[test_ind_dict['budget-'+str(i)]] = preds_budget[i]
-    for i in range(len(preds_no_budget)):
-        preds[test_ind_dict['none-'+str(i)]] = preds_no_budget[i]
-
-    pred_lst = [0 if preds[i] < 0 else math.e**preds[i]
-                for i in range(test_len)]
-
-    '''measure = 0.
-    for i in range(test_len):
-        measure += math.fabs(pred_lst[i] - train_regy[i])
-    print measure/test_len'''
-
-    #plt.plot(test_screens,pred_lst,'bo')
-    #plt.show()
-
-    util.write_predictions(pred_lst, test_ids, 'screens_log_budget_log.csv')
-
-def product(x, coeffs, basis_fns):
-    out = 0.
-    for i in range(len(basis_fns)):
-        out += coeffs[i]*basis_fns[i](x)
-    return out
-
-def screens_budget_summer():
-    train_mat,key,train_regy,_ = rs.extract_feats([rs.metadata_feats])
-    train_len = train_mat.shape[0]
-    summer_lst = []
-    no_summer_lst = []
+def screens(basis_fns, fns, inv_fn, outfile):
+    mat,key,regy,_ = rs.extract_feats([rs.metadata_feats])
     screen_ind = key['number_of_screens']
-    budget_ind = key['production_budget']
-    for i in range(train_len):
-        if train_mat[i,key['summer_release']] == 1.:
-            summer_lst.append([train_mat[i,screen_ind],train_mat[i,budget_ind],
-                               train_regy[i]])
-        else:
-            no_summer_lst.append([train_mat[i,screen_ind],
-                                  train_mat[i,budget_ind],train_regy[i]])
-    summer_mat = np.array(summer_lst)
-    no_summer_mat = np.array(no_summer_lst)
+    screens = mat.getcol(screen_ind).todense()
+    train_arr = format_arr([screens], regy, fns)
+    coeffs = freg.coeffs(basis_fns, train_arr)
 
-    basis_fns_budget = [lambda (x,y):1,lambda (x,y):x,lambda (x,y):x**2,
-                        lambda (x,y):y, lambda (x,y):y**2]
-    basis_fns_no_budget = [lambda x:1, lambda x:x, lambda x:x**2]
-
-    def split(mat):
-        screens = mat[:,0]
-        budget = mat[:,1]
-        regy = mat[:,2]
-        mlen = mat.shape[0]
-
-        budget_lst = []
-        no_budget_lst = []
-        for i in range(mlen):
-            if budget[i] > 0.:
-                budget_lst.append([(math.log(screens[i]),
-                                    math.log(budget[i])),
-                                   math.log(regy[i])])
-            else:
-                no_budget_lst.append([math.log(screens[i]),math.log(regy[i])])
-
-        budget_arr = np.array(budget_lst)
-        no_budget_arr = np.array(no_budget_lst)
-        coeffs1 = reg.coeffs(basis_fns_budget,budget_arr)
-        coeffs2 = reg.coeffs(basis_fns_no_budget,no_budget_arr)
-        return (coeffs1,coeffs2)
-                
-    s_b_coeffs, s_nb_coeffs = split(summer_mat)
-    ns_b_coeffs, ns_nb_coeffs = split(no_summer_mat)
-
-    test_mat,_,_,test_ids = rs.extract_feats([rs.metadata_feats],'testcases.xml',
-                                             global_feat_dict = key)
-    test_len = test_mat.shape[0]
+    test,_,_,ids = rs.extract_feats([rs.metadata_feats],'testcases.xml',
+                                    global_feat_dict = key)
+    test_len = test.shape[0]
     preds = []
     for i in range(test_len):
-        if test_mat[i,key['summer_release']] == 0.:
-            if test_mat[i,key['production_budget']] == 0.:
-                preds.append(product(math.log(test_mat[i,key['number_of_screens']]),
-                                     ns_nb_coeffs,basis_fns_no_budget))
-            else:
-                preds.append(product((math.log(test_mat[i,key['number_of_screens']]),
-                                      math.log(test_mat[i,key['production_budget']])),
-                                     ns_b_coeffs,basis_fns_budget))
+        prod = freg.product((fns[0](test[i,screen_ind]),), coeffs, basis_fns)
+        if prod < 0:
+            prod = 0
+        preds.append(inv_fn(prod))
+    util.write_predictions(preds, ids, outfile)
+
+def screens_idid():
+    screens([lambda x:1, lambda x:x[0]], [lambda x:x, lambda x:x], lambda x:x,
+            'screens_idid-2.csv')
+
+def screens_idlog():
+    screens([lambda x:1, lambda x:x[0], lambda x:x[0]**2],
+            [lambda x:x, lambda x:math.log(x)], lambda x:math.e**x, 'screens_idlog-2.csv')
+
+def screens_loglog():
+    screens([lambda x:1, lambda x:x[0], lambda x:x[0]**2],
+            [lambda x:math.log(x), lambda x:math.log(x)], lambda x:math.e**x,
+            'screens_loglog-2.csv')
+
+def screens_budget_lglglg():
+    mat,key,regy,_ = rs.extract_feats([rs.metadata_feats])
+    screen_ind = key['number_of_screens']
+    budget_ind = key['production_budget']
+    screens = mat.getcol(screen_ind).todense()
+    budget = mat.getcol(budget_ind).todense()
+
+    budget_fns = [lambda x:math.log(x) for i in range(3)]
+    budget_check = lambda x:x[1] > 0.
+    budget_arr = format_arr([screens,budget], regy, budget_fns, budget_check)
+    no_budget_arr = format_arr([screens],regy,[lambda x:math.log(x),lambda x:math.log(x)])
+
+    budget_basis_fns = [lambda x:1, lambda x:x[0], lambda x:x[0]**2,
+                        lambda x:x[1], lambda x:x[1]**2]
+    no_budget_basis_fns = [lambda x:1, lambda x:x[0], lambda x:x[0]**2]
+
+    budget_coeffs = freg.coeffs(budget_basis_fns, budget_arr)
+    no_budget_coeffs = freg.coeffs(no_budget_basis_fns, no_budget_arr)
+
+    test,_,_,ids = rs.extract_feats([rs.metadata_feats],'testcases.xml',
+                                    global_feat_dict = key)
+    test_len = test.shape[0]
+    preds = []
+    for i in range(test_len):
+        prod = 0
+        if test[i,budget_ind] > 0.:
+            x = (budget_fns[0](test[i,screen_ind]), budget_fns[1](test[i,budget_ind]))
+            prod = freg.product(x, budget_coeffs, budget_basis_fns)
         else:
-            if test_mat[i,key['production_budget']] == 0.:
-                preds.append(product(math.log(test_mat[i,key['number_of_screens']]),
-                                     s_nb_coeffs,basis_fns_no_budget))
-            else:
-                preds.append(product((math.log(test_mat[i,key['number_of_screens']]),
-                                      math.log(test_mat[i,key['production_budget']])),
-                                     s_b_coeffs,basis_fns_budget))
+            x = (math.log(test[i,screen_ind]),)
+            prod = freg.product(x, no_budget_coeffs, no_budget_basis_fns)
+        if prod < 0:
+            prod = 0
+        preds.append(math.e**prod)
+    util.write_predictions(preds, ids, 'screens_budget_lglglg-2.csv')
 
-    pred_lst = [1 if preds[i] < 0 else math.e**preds[i] for i in range(test_len)]
+screens_budget_lglglg()
 
-    util.write_predictions(pred_lst, test_ids, 'screens_budget_summer.csv')
+def screens_budget_summer_lglglg():
+    mat,key,regy,_ = rs.extract_feats([rs.metadata_feats])
+    screen_ind = key['number_of_screens']
+    budget_ind = key['production_budget']
+    summer_ind = key['summer_release']
 
-screens_budget_summer()
+    screens = mat.getcol(screen_ind).todense()
+    budget = mat.getcol(budget_ind).todense()
+    summer = mat.getcol(summer_ind).todense()
+
+    def safelog(x):
+        if x <= 0.:
+            return 0.
+        else:
+            return math.log(x)
+    fns = [lambda x : if x <= 0.math.log(x)]
+    bs_check = lambda x:x[1] > 0. && x[2] > 0.
+    bns_check = lambda x:x[1] > 0. && x[2] == 0.
+    nbs_check = lambda x:x[1] == 0. && x[2] > 0.
+    nbns_check = lambda x:x[1] == 0. && x[2] == 0.
+
+    budget_arr = format_arr([screens,budget], regy, budget_fns, budget_check)
+    no_budget_arr = format_arr([screens],regy,[lambda x:math.log(x),lambda x:math.log(x)])
+
+    budget_basis_fns = [lambda x:1, lambda x:x[0], lambda x:x[0]**2,
+                        lambda x:x[1], lambda x:x[1]**2]
+    no_budget_basis_fns = [lambda x:1, lambda x:x[0], lambda x:x[0]**2]
+
+    budget_coeffs = freg.coeffs(budget_basis_fns, budget_arr)
+    no_budget_coeffs = freg.coeffs(no_budget_basis_fns, no_budget_arr)
+
+    test,_,_,ids = rs.extract_feats([rs.metadata_feats],'testcases.xml',
+                                    global_feat_dict = key)
+    test_len = test.shape[0]
+    preds = []
+    for i in range(test_len):
+        prod = 0
+        if test[i,budget_ind] > 0.:
+            x = (budget_fns[0](test[i,screen_ind]), budget_fns[1](test[i,budget_ind]))
+            prod = freg.product(x, budget_coeffs, budget_basis_fns)
+        else:
+            x = (math.log(test[i,screen_ind]),)
+            prod = freg.product(x, no_budget_coeffs, no_budget_basis_fns)
+        if prod < 0:
+            prod = 0
+        preds.append(math.e**prod)
+    util.write_predictions(preds, ids, 'screens_budget_lglglg-2.csv')
