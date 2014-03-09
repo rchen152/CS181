fecc92135a6888fcde0547d521920a84d889f411
Rebecca Chen <rchen@college.harvard.edu>
2014-03-09 16:59:27 -0400

    system calls by type of malware

--- practical3/extract.py
+++ practical3/extract.py
@@ -1,6 +1,7 @@
 import classification_starter as classify
 from collections import Counter
 import matplotlib.pyplot as plt
+import numpy as np
 
 def structure(tree):
     c = Counter()
@@ -17,10 +18,39 @@ def structure(tree):
             c['num_syscalls'] += 1
     return c
 
-mat,key,cats,_ = classify.extract_feats([structure], 'train')
+def syscalls(tree):
+    bad_tags = ['processes', 'process', 'thread', 'all_section']
+    calls = {}
+    for node in tree.iter():
+        if node.tag in bad_tags:
+            continue
+        if node.tag not in calls:
+            calls[node.tag] = 1.
+        else:
+            calls[node.tag] += 1.
+    return calls
+
+def example_structure_plot():
+    mat,key,cats,_ = classify.extract_feats([structure], 'train')
+    for i in range(mat.shape[0]):
+        color = 'red'
+        if cats[i] == 8:
+            color = 'black'
+        plt.scatter([mat[i,key['num_processes']]], [cats[i]], c=color)
+    plt.show()
+
+mat,key,cats,ids = classify.extract_feats([syscalls], 'train')
+counts = np.asarray(mat.sum(axis=0))[0]
+prop_mat = np.zeros((16, mat.shape[1]))
+prop_mat[prop_mat.shape[0]-1] = counts
+cat_counts = np.zeros((16))
+cat_counts[15] = mat.shape[0]
 for i in range(mat.shape[0]):
-    color = 'red'
-    if cats[i] == 8:
-        color = 'black'
-    plt.scatter([mat[i,key['num_processes']]], [cats[i]], c=color)
-plt.show()
+    prop_mat[cats[i]] += mat[i]
+    cat_counts[cats[i]] += 1
+for i in range(prop_mat.shape[0]):
+    prop_mat[i] /= cat_counts[i]
+    out = ''
+    for x in prop_mat[i]:
+        out += str(x) + '\t'
+    print out[:-1]
