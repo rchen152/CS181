ae9f2db1afdfcba420e3f306b0c7a8bbf44bfa78
Aaron Landesman <aaronlandesman@gmail.com>
2014-05-01 17:37:44 -0400

    better pickled tree

--- final/train/ghost_tree.py
+++ final/train/ghost_tree.py
@@ -17,12 +17,15 @@ test = test_matrix[:,3:]
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(mat,cats)
 
-#pickle.dump(clf,open("pickled_tree.p","wb"))
+#pickle.dump(clf,open("pickled_tree_101_200.p","w"))
 preds = clf.predict(test)
 print preds
 correct_preds = test_matrix[:,1]
 print correct_preds
-lst = [correct_preds[i]==preds[i] for i in range(len(preds))]
+lst = [preds[i] for i in range(len(preds)) if correct_preds[i]==preds[i]]
+
+for i in range(6):
+    print len(filter(lambda x: x ==float(i),filter(correct_preds[i]==preds[i],preds)))
 for i in range(6):
     print len(filter(lambda x: x ==float(i),correct_preds))
 print len(lst)
