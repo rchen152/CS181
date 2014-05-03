ea3169013ea06e1f3c1d55ef844b3a9833c2998e
Aaron Landesman <aaronlandesman@gmail.com>
2014-05-03 18:00:36 -0400

    added bad ghost tracker

--- final/ExampleTeam/studentAgents.py
+++ final/ExampleTeam/studentAgents.py
@@ -9,6 +9,11 @@ import pickle
 import classify
 
 GOOD_CAPS_CSV = 'data/good_caps_train.csv'
+GAME_LEN = 1000
+BAD_QUAD = 4
+NUM_GHOSTS = 4
+bad_ghost_vec = np.array([])
+previous_ghost_state = np.array([])
 
 class BaseStudentAgent(object):
     """Superclass of agents students will write"""
@@ -38,11 +43,14 @@ class CoequalizerAgent(BaseStudentAgent):
     (and also renaming it in registerInitialState() below), modify the behavior
     of this class so it does well in the pacman game!
     """
-    
+
+
+
     def __init__(self, *args, **kwargs):
         """
         arguments given with the -a command line option will be passed here
         """
+
         pass # you probably won't need this, but just in case
     
     def registerInitialState(self, gameState):
@@ -58,7 +66,44 @@ class CoequalizerAgent(BaseStudentAgent):
         # learned_params = np.load("myparams.npy") 
         
         ghost_params = pickle.load(open('train/pickled_tree_101_200.p','r'))
-    
+
+
+    def badFeature(self, ghostState,observedState):
+        badghosts = filter(lambda x: ObservedState.getGhostQuadrant(observedState,x) == BAD_QUAD,ghostState)
+        if(len(badghosts)<1):
+            print "fewer bad ghosts than expected error"
+            return None
+        elif(len(badghosts) > 1):
+            print "more bad ghosts than expected error"
+        else:    
+            return badghosts[0].getFeatures()
+
+    def updateBadGhost(self, observedState):
+        global bad_ghost_vec
+        global previous_ghost_state
+        
+        ghost_states = observedState.getGhostStates() # states have getPosition() and getFeatures() methods
+        if(GAME_LEN == observedState.getNumMovesLeft()):
+            return self.badFeature(ghost_states,observedState)
+
+        ghost_features = map(lambda x : x.getFeatures(),ghost_states)
+
+        if (filter(lambda x : (x == bad_ghost_vec).all(), ghost_features) == []):
+            possible_ghosts = filter(lambda x: ObservedState.getGhostQuadrant(observedState,x) == BAD_QUAD,ghost_states)
+            if(len(possible_ghosts)<1):
+                print "error no quad 4 ghosts"
+                return np.array([])
+            if(len(possible_ghosts) == 1):
+                return possible_ghosts[0].getFeatures()
+            if(len(possible_ghosts)>1):
+                prev_ghost_features = map(lambda x : x.getFeatures() , previous_ghost_state)
+                b_g_candidates = filter(lambda x : (filter(lambda y : (y==x).all(),prev_ghost_features)==[]), possible_ghosts)
+                if(len(b_g_candidates) != 1):
+                    print "not exactly one ghost regenerated in quadrant 4 error"
+                    return np.array([])
+                else: return b_g_candidates[0].getFeatures()
+        else:
+            return bad_ghost_vec
     def chooseAction(self, observedState):
         """
         Here, choose pacman's next action based on the current state of the game.
@@ -68,9 +113,23 @@ class CoequalizerAgent(BaseStudentAgent):
         to. This is not a very good strategy, and completely ignores the features of
         the ghosts and the capsules; it is just designed to give you an example.
         """
+        global bad_ghost_vec
+        global previous_ghost_state
+
+
+        ghost_states = observedState.getGhostStates() # states have getPosition() and getFeatures() methods
+        ghost_features = map(lambda x : x.getFeatures(),ghost_states)
+        if(len(ghost_features) != NUM_GHOSTS):
+            print "unexpected number of ghosts" + str(len(ghost_features))
+
+        bad_ghost_vec = self.updateBadGhost(observedState)
+        bad_ghost = filter(lambda x: (bad_ghost_vec == x.getFeatures()).all(),ghost_states)[0]
+        print ObservedState.getGhostQuadrant(observedState,bad_ghost)
+        previous_ghost_state = ghost_states
+
         pacmanPosition = observedState.getPacmanPosition()
-        ghost_states = observedState.getGhostStates() # states have getPosition() and get Features() methods
-        ghost_features = ghost_states.getFeatures()
+
+
         legalActs = [a for a in observedState.getLegalPacmanActions()]
         ghost_dists = np.array([self.distancer.getDistance(pacmanPosition,gs.getPosition()) 
                               for gs in ghost_states])
