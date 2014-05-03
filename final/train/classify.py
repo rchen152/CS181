eb955853678ad1185f241d9bdd4060d4d2eb7dc6
Rebecca Chen <rchen@college.harvard.edu>
2014-05-03 17:46:21 -0400

    pulling own changes

--- final/train/classify.py
+++ final/train/classify.py
@@ -1,4 +1,5 @@
 import cPickle as pickle
+import numpy as np
 from scipy.cluster.vq import kmeans, vq
 
 fPCA = open('../train/pca.pickle', 'r')
@@ -9,10 +10,16 @@ fKMeans = open('../train/kmeans_centers.pickle', 'r')
 centers = pickle.load(fKMeans)
 fKMeans.close()
 
+good_data = pca.transform(np.loadtxt('../data/good_cap.csv'))
+good_pt = good_data[0].reshape((1,good_data.shape[1]))
+cats,_ = vq(good_pt,centers)
+good_index = cats[0]
+
 '''fTree = open('tree.pickle', 'r')
 tree = pickle.load(fTree)
 fTree.close()
 '''
 def cap_classify(data):
+    data = pca.transform(data)
     cats,_ = vq(data,centers)
-    return [1 if cat==2 else 0 for cat in cats]
+    return [1 if cat==good_index else 0 for cat in cats]
