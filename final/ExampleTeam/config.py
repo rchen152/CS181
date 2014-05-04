916bcc107ccc658f710b8c8df220b6cad1d3cd33
Rebecca Chen <rchen@college.harvard.edu>
2014-05-04 19:41:08 -0400

    ExampleTeam changed to CoequalizerTeam

--- final/ExampleTeam/config.py
+++
@@ -1,5 +0,0 @@
-teamname = 'Coequalizer'
-
-members = [ ('Aaron Landesman', 'aaronlandesman@gmail.com'),
-            ('Spencer Kwon', 'yongwookkwon@college.harvard.edu'),
-            ('Rebecca Chen', 'rchen@college.harvard.edu')]
