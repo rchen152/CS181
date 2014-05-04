9e5e198bdca05b8c233b9ad717d6dd304a37434a
Aaron Landesman <aaronlandesman@gmail.com>
2014-05-04 02:41:25 -0400

    wrote data collection agent assuming state function

--- final/ExampleTeam/studentAgents.py
+++ final/ExampleTeam/studentAgents.py
@@ -5,8 +5,10 @@ from observedState import ObservedState
 import numpy as np
 import csv
 from util import random, manhattanDistance, Counter, chooseFromDistribution, raiseNotDefined
-import pickle
+import cPickle as pickle
 import classify
+
+
 
 GOOD_CAPS_CSV = 'data/good_caps_train.csv'
 GAME_LEN = 1000
@@ -15,6 +17,12 @@ NUM_GHOSTS = 4
 bad_ghost_vec = np.array([])
 previous_ghost_state = np.array([])
 avg_class_juice =[28.867748179685883, 52.257299401447447, 153.57566648602614, 17.2900555038538, 0,0]
+
+num_states = 3861
+num_actions = 4
+sa_ds = pickle.load(open('ExampleTeam/pickled_sa.p','r'))
+
+dir_dict = {Directions.NORTH:0,Directions.SOUTH:1,Directions.EAST:2,Directions.WEST:3,Directions.STOP:4}
 
 class BaseStudentAgent(object):
     """Superclass of agents students will write"""
@@ -100,23 +108,17 @@ class CoequalizerAgent(BaseStudentAgent):
                 prev_ghost_features = map(lambda x : x.getFeatures() , previous_ghost_state)
                 b_g_candidates = filter(lambda x : (filter(lambda y : (y==x).all(),prev_ghost_features)==[]), possible_ghosts)
                 if(len(b_g_candidates) != 1):
-                    print "not exactly one ghost regenerated in quadrant 4 error"
+                    print "not exactly one ghost regenerated in quadrant  4 error"
                     return np.array([])
                 else: return b_g_candidates[0].getFeatures()
         else:
             return bad_ghost_vec
 
     def chooseAction(self, observedState):
-        """
-        Here, choose pacman's next action based on the current state of the game.
-        This is where all the action happens.
-        
-        This silly pacman agent will move away from the ghost that it is closest
-        to. This is not a very good strategy, and completely ignores the features of
-        the ghosts and the capsules; it is just designed to give you an example.
-        """
+
         global bad_ghost_vec
         global previous_ghost_state
+
 
 
         ghost_states = observedState.getGhostStates() # states have getPosition() and getFeatures() methods
@@ -127,45 +129,93 @@ class CoequalizerAgent(BaseStudentAgent):
         bad_ghost_vec = self.updateBadGhost(observedState)
         bad_ghost = filter(lambda x: (bad_ghost_vec == x.getFeatures()).all(),ghost_states)[0]
         print ObservedState.getGhostQuadrant(observedState,bad_ghost)
+
+
+
+
+
         previous_ghost_state = ghost_states
-
-
-        pacmanPosition = observedState.getPacmanPosition()
-
-
-        legalActs = [a for a in observedState.getLegalPacmanActions()]
-        ghost_dists = np.array([self.distancer.getDistance(pacmanPosition,gs.getPosition()) 
-                              for gs in ghost_states])
-        # find the closest ghost by sorting the distances
-        closest_idx = sorted(zip(range(len(ghost_states)),ghost_dists), key=lambda t: t[1])[0][0]
-        # take the action that minimizes distance to the current closest ghost
-        best_action = Directions.STOP
-        best_dist = -np.inf
-        for la in legalActs:
-            if la == Directions.STOP:
-                continue
-            successor_pos = Actions.getSuccessor(pacmanPosition,la)
-            new_dist = self.distancer.getDistance(successor_pos,ghost_states[closest_idx].getPosition())
-            if new_dist > best_dist:
-                best_action = la
-                best_dist = new_dist
+#Rewrite what we return
         return best_action
-    
+
+class CollectAgent(BaseStudentAgent):
+
+    def explore(self,observedState):
+        global sa_ds
+        global previous_state
+        global previous_action
+        global old_score
+
+        old_s = self.getStateNum(previous_ghost_state)
+        new_s = self.getStateNum(observedState)
+        
+        
+        if new_s in sa_ds[old_s * 4 + dir_dict[previous_action]]:
+            sa_ds[old_s * 4 + dir_dict[previous_action]][new_s] += 1
+        else:
+            sa_ds[old_s * 4 + dir_dict[previous_action]][new_s] = 1
+
+        sa_ds[old_s * 4 + dir_dict[previous_action]]['count']+=1
+
+        sa_ds[old_s * 4 + dir_dict[previous_action]]['total_reward']+= (old_score - observedState.getScore())
+        
+    def chooseAction(self,observedState):
+        global previous_state
+        global previous_action
+        global old_score
+        
+        legalActs = [a for a in observedState.getLegalPacmanActions()]
+        if (legalActs == Directions.STOP):
+            print "error you are trapped by four walls"
+            previous_state = observedState
+            old_score = observedState.getScore()
+            return Directions.STOP
+
+        if (observedState.getNumMovesLeft == 1):
+            pickle.dump(sa_ds,open("pickled_sa.p","w"))
+
+        if (observedState.getNumMovesLeft != GAME_LEN):
+            self.explore(observedState)        
+            
+        fil_legal = legalActs.remove(Directions.STOP)
+        s = self.getNumState(observedState)
+        count_lst = [sa_ds[4*s + dir_dict[fil_legal[i]]]['count'] for i in range(len(fil_legal))]
+        previous_state = observedState
+        old_score = observedState.getScore()
+        
+        act = fil_legal[count_lst.index(min(count_lst))]
+        previous_action = act
+
+        if(s == 0):
+            
+
+        return act
+
+
 class FuturePosAgent(BaseStudentAgent):
     def chooseAction(self, observedState):
         legalActs = [a for a in observedState.getLegalPacmanActions()]
-        g_pos = map(lambda x : x.getPosition(),observedState.getGhostStates())
-        print g_pos
-#        print ObservedState.pacmanFuturePosition(observedState,[Directions.WEST])
-#       print ObservedState.pacmanFuturePosition(observedState,[Directions.WEST,Directions.WEST,Directions.WEST])
+#        print legalActs
+        act = random.choice(legalActs)
+#        print (act,response_lst[dir_dict[act]])
+
+#        g_pos = map(lambda x : x.getPosition(),observedState.getGhostStates())
+#        p_pos = observedState.getPacmanPosition()
+#        if(filter(lambda x: x == p_pos,g_pos) != []):
+#            print g_pos
+#            print p_pos
+        print observedState.getNumMovesLeft()
+        return act
+'''        print ObservedState.pacmanFuturePosition(observedState,[Directions.WEST])
+       print ObservedState.pacmanFuturePosition(observedState,[Directions.WEST,Directions.WEST,Directions.WEST])
         dir_lst = [Directions.NORTH,Directions.SOUTH,Directions.EAST,Directions.WEST,Directions.STOP]
-        dir_dict = {Directions.NORTH:0,Directions.SOUTH:1,Directions.EAST:2,Directions.WEST:3,Directions.STOP:4}
+
         fst_g_pos = observedState.getGhostStates()[0].getPosition()
         response_lst = [ObservedState.ghostFuturePosition(observedState,0,[i]) for i in dir_lst]
-        print response_lst
-        act = random.choice(legalActs)
-        print (act,response_lst[dir_dict[act]])
-        return act                
+        print response_lst'''
+        
+
+
 
 class DataAgent(BaseStudentAgent):
 
@@ -185,8 +235,8 @@ class DataAgent(BaseStudentAgent):
 
 
 ## Below is the class students need to rename and modify
-'''
 
+'''
 class ExampleTeamAgent(BaseStudentAgent):
     """
     An example TeamAgent. After renaming this agent so it is called <YourTeamName>Agent,
@@ -239,5 +289,6 @@ class ExampleTeamAgent(BaseStudentAgent):
             if new_dist > best_dist:
                 best_action = la
                 best_dist = new_dist
+            if new_dist < 2: print new_dist
         return best_action
 '''
